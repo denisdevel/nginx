cd28ed509d3db12aff6f58b00f228569bc1defd6
Jenkins Automation Server <jenkins@ip-172-31-33-247.ec2.internal>
2018-04-04 11:35:46 +0000

    123

--- docker-machine.sh
+++ docker-machine.sh
@@ -6,7 +6,7 @@ eval $(/usr/local/bin/docker-machine env opsworks)
 echo " try to remove all running containers"
 for i in `docker ps -a | sed -e s/\ // | awk '{print $1}'i | sed -n '2,$p'`;do docker rm -f $i; done && 
 echo "get last image"
-docker pull najar/nginx
+docker rmi najar/nginx && docker pull najar/nginx
 echo "running docker container"
 docker run -d -p 80:80 najar/nginx
 
