0c94ecf0b80dcc3bf7e83f359bcc1fae8b7e6705
Jenkins Automation Server <jenkins@ip-172-31-33-247.ec2.internal>
2018-04-04 05:54:16 +0000

    add files

--- script.sh
+++ script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 SERVICE_NAME="nginx-service"
 IMAGE_VERSION="v_"${BUILD_NUMBER}
-TASK_FAMILY="nginx-family"
+TASK_FAMILY="nginx"
 
 # Create a new task definition for this build
 sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" nginx.json > nginx-v_${BUILD_NUMBER}.json
-aws ecs register-task-definition --family nginx-family --cli-input-json file://nginx-v_${BUILD_NUMBER}.json
+aws ecs register-task-definition --family nginx --cli-input-json file://nginx-v_${BUILD_NUMBER}.json
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task-definition nginx-family | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
-DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
+TASK_REVISION=`aws ecs describe-task-definition --task-definition nginx | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
+DESIRED_COUNT=`aws ecs describe-services --cluster joomla --services ${SERVICE_NAME} | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//' | head -1`
 if [ ${DESIRED_COUNT} = "0" ]; then
     DESIRED_COUNT="1"
 fi
 
-aws ecs update-service --cluster default --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
+LOG1="aws ecs update-service --cluster joomla --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}"
+echo ${LOG1}
+aws ecs update-service --cluster joomla --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
